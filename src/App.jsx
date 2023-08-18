0c14da4e960404fee3c9181a5871673c5c892c6d
VikashMaurya10 <VikashMaurya10stp@gmail.com>
2023-08-18 16:08:28 +0530

    fix all to list and set env

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import InsertTodo from './components/InsertTodo'
-import GetAllTodos from './components/GetAllTodos'
+// import GetAllTodos from './components/GetAllTodos'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-
-
-
   return (
     <main className='flex flex-col justify-center items-center bg-gray-400 h-screen'>
       <InsertTodo />
-      <GetAllTodos />
+      {/* <GetAllTodos /> */}
+
+
       <ToastContainer
         position="top-right"
         autoClose={1000}
